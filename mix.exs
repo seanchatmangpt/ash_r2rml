198415370dd9550a5c1fa024cad66f215663f044
Matt Beanland <beanland@live.com.au>
2026-06-21 22:47:30 +0930

    chore: release version v0.10.1

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule AshNeo4j.MixProject do
   @moduledoc false
   use Mix.Project
 
-  @version "0.10.0"
+  @version "0.10.1"
   @name "AshNeo4j"
   @description "Ash DataLayer for Neo4j"
   @github_url "https://github.com/diffo-dev/ash_neo4j"
