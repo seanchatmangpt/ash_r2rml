9da20049551e29f41d7e28cd43d01039366345f2
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-08-27 20:52:10 -0700

    test(ws5): guard AshR2RML package version

---
+++ test/ws5_contracts/version_test.exs
@@ -0,0 +1,8 @@
+defmodule AshR2RML.WS5.VersionContractTest do
+  use ExUnit.Case, async: true
+  @manifest File.read!("mix.exs")
+
+  test "package version remains 26.8.25" do
+    assert @manifest =~ ~s(@version "26.8.25")
+  end
+end
