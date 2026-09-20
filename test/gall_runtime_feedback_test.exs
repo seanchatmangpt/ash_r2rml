f4e2bba783ee6d2e27d655c562a659b6b18a2a7f
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-09-19 23:46:05 -0700

    chore(gall-009): add REUSE metadata

--- test/gall_runtime_feedback_test.exs
+++ test/gall_runtime_feedback_test.exs
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2026 ash_r2rml contributors <https://github.com/seanchatmangpt/ash_r2rml/graphs/contributors>
+#
+# SPDX-License-Identifier: MIT
+
 defmodule AshR2RML.Gall.RuntimeFeedbackTest do
   use ExUnit.Case, async: true
 
